ef6bc319144ac57d3720c2a9a7bf21e4ad9a1b99
OEivind Skovborg <oivind@skovborg.com>
2012-08-28 17:41:34 +0200

    Added new end point for sending message

---
+++ src/FubuDate/Domain/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FubuDate.Domain
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+        public DateTime Sent { get; set; }
+    }
+}
---
+++ src/FubuDate/Endpoints/Message/IndexEndpoint.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using Raven.Client;
+
+namespace FubuDate.Endpoints.Message
+{
+    public class IndexEndpoint
+    {
+        private readonly IDocumentSession _session;
+
+        public IndexEndpoint(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        public MessageIndexViewModel Get(MessageIndexRequest request)
+        {
+            return new MessageIndexViewModel{From = request.From};
+        }
+
+        public MessageIndexViewModel Post(MessageIndexRequest request)
+        {
+            var message = new Domain.Message
+                              {
+                                  Body = request.Message,
+                                  To = request.To,
+                                  From = request.From,
+                                  Sent = DateTime.Now
+                              };
+
+            _session.Store(message);
+
+            return new MessageIndexViewModel {IsMessageSent = true, From = request.From};
+        }
+    }
+}
